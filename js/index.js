f482f6ce858a48c3d8ab918338fc4d0d70f8ca53
mathlete01 <msallin@gmail.com>
2020-11-23 08:40:03 -0800

    Return JSON object and print titles to list

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+
+    let url = "http://localhost:3000/books"
+
+    let listPanel = document.getElementById("list-panel")
+
+    function createList(books){
+        //console.dir(`books[1].title = ${books[1].title}`)
+        let ul = document.createElement("ul")
+        for(let i = 0; i < books.length; i ++){
+            let book = books[i]
+            let li = document.createElement("li")
+            li.innerText = book.title
+            listPanel.appendChild(li)
+        }
+    }
+
+    function fetchContent(url){
+        fetch(url)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(response){
+            jsonObj = response
+            createList(jsonObj)
+        })
+    }
+
+    fetchContent(url)
+
+});
