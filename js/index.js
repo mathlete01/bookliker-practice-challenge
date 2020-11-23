4dca233879bf6e8220bf4d3d431c42bf0dbe35ba
mathlete01 <msallin@gmail.com>
2020-11-23 14:08:55 -0800

    All works except like btn

--- js/index.js
+++ js/index.js
@@ -5,34 +5,52 @@ document.addEventListener("DOMContentLoaded", function() {
     const listPanel = document.getElementById("list-panel")
     const showPanel = document.getElementById("show-panel")
 
-    function showBook(event){
-        //console.log(`showBook called`)
-        console.log(`event.target.id = ${event.target.id}`)
-        // let img = document.createElement("image")
-        // let pTitle = document.createElement("p")
-        // let pSubtitle = document.createElement("p")
-        // let pAuthor = document.createElement("p")
-        // let pDesc = document.createElement("p")
-        // let ulUsers = document.createElement("ul")
-        // let likeBtn = document.createElement("button")
+    function showBook(book){
+        const showUL = document.createElement("ul")
+        let image = document.createElement("img")
+        let pTitle = document.createElement("li")
+        let pSubtitle = document.createElement("p")
+        let pAuthor = document.createElement("p")
+        let pDesc = document.createElement("p")
+        let ulUsers = document.createElement("ul")
+        let likeBtn = document.createElement("button")
 
-        // img.src = book.img_url
-        // pTitle.innerText = book.title
-        // pSubtitle.innerText = book.subtitle
-        // pAuthor.innerText = book.author
-        // pDesc.innerText = book.description
-        // ulUsers.innerText = "Users"
+        image.src = book.img_url
+        pTitle.innerText = book.title
+        pSubtitle.innerText = book.subtitle
+        pAuthor.innerText = book.author
+        pDesc.innerText = book.description
+        ulUsers.innerText = "Users"
+        likeBtn.innerText = "Like"
 
-        // showPanel.appendChild(img, pTitle)
+        showPanel.innerHTML = ""
+
+        let users = book.users
+        for (user of users){
+            console.log(`user.username = ${user.username}`)
+            let userLI = document.createElement("li")
+            userLI.innerText = user.username
+            ulUsers.appendChild(userLI)
+        }
+
+        showUL.appendChild(pTitle)
+        showPanel.appendChild(showUL)
+        showPanel.appendChild(image)
+        showPanel.appendChild(pAuthor)
+        showPanel.appendChild(pSubtitle)
+        showPanel.appendChild(pDesc)
+        showPanel.appendChild(ulUsers)
+        showPanel.appendChild(likeBtn)
     }
 
     function createList(books){
         const ul = document.createElement("ul")
-        ul.addEventListener("click", (event) => showBook(event))
         for(let i = 0; i < books.length; i ++){
             let book = books[i]
             let li = document.createElement("li")
-            li.setAttribute("id", book.id)
+            // Event listener requires a reference to a function, not an invoked function. If no argument needed, I could reference it without inoking it with simply showBook and no parens
+            //If I need to pass an argument, I to wrap it in an anonymous functoin 
+            li.addEventListener("click", () => showBook(book))
             li.innerText = book.title
             ul.appendChild(li)
         }
